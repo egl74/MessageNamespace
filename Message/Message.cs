4ee598b1997f9940704f668141010fad372372d7
egl74 <laptenauck@gmail.com>
2015-03-31 17:45:02 +0300

    number one

---
+++ Message/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageNamespace
+{
+    [Serializable]
+    public class Message
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string Result { get; set; }
+        public ActionType Action { get; set; }
+    }
+
+    [Serializable]
+    public enum ActionType
+    {
+        SearchByWord, SearchByLetter, Add, Edit, Remove
+    }
+
+    
+}
